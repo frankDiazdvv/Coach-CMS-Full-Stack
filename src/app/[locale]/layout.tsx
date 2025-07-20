b614ab2879a864af7e4805ff20a56eb8a2fa3637
frankDiazdvv <158342906+frankDiazdvv@users.noreply.github.com>
2025-07-20 13:04:22 -0400

    Used layout from next-intl docs

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,30 +1,25 @@
-import { NextIntlClientProvider, hasLocale } from 'next-intl';
-import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
-import './globals.css';
-
-export async function generateStaticParams(): Promise<Array<{ locale: string }>> {
-  return routing.locales.map((locale) => ({ locale }));
-}
-
-type LayoutProps = {
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+import './globals.css'
+ 
+export default async function LocaleLayout({
+  children,
+  params
+}: {
   children: React.ReactNode;
-  params: { locale: string };
-};
-
-export default async function LocaleLayout({ children, params }: LayoutProps) {
-  const { locale } = params;
-
+  params: Promise<{locale: string}>;
+}) {
+  // Ensure that the incoming `locale` is valid
+  const {locale} = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
-
+ 
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider locale={locale}>
-          {children}
-        </NextIntlClientProvider>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
   );
