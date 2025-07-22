6a81e15ba684bfacc7402f0a4b30a353491465f7
frankDiazdvv <158342906+frankDiazdvv@users.noreply.github.com>
2025-07-21 21:46:21 -0400

    fixed language

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,24 +2,26 @@ import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import './globals.css'
- 
+
 export default async function LocaleLayout({
   children,
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: {locale: string}; // ✅ NOT a Promise
 }) {
-  // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
+  const {locale} = params;
+
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale} messages={(await import(`../../../messages/${locale}.json`)).default}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
